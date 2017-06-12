3d7debf29ebdc6c0d253ae506dcea542df76b257
HawiCaesar <brian.odhiambo932@gmail.com>
2017-06-12 18:15:11 +0300

    added correct result test and program logic for prime numbers

--- generate_prime_numbers.py
+++ generate_prime_numbers.py
@@ -1,12 +1,34 @@
+"""
+Generate prime numbers from 0 to n with asymtotic analysis
+
+"""
+
 def generate_prime_numbers_asymptotic(last_number):
 	prime_number_list = []
 
-	if(last_number == 1):
-		return 1
+	if last_number < 0:
+		return "Only postive integers are allowed"
+
+	elif last_number < 2:
+		return "Numbers below the number you have entered do not have prime numbers"
+
 	else:
 
 		for number in range(2, last_number + 1):
+			for i in range(2, number):
 
-			if last_number % number != 0:
-				
+				## If not a prime number
+				if number%i == 0:
+					break
+			## All others are prime
+			else:
+				prime_number_list.append(number)
+
+
+		return prime_number_list
+
+
+
+print(generate_prime_numbers_asymptotic(20))
+
 
--- test_generate_prime_numbers_asymtotic.py
+++ test_generate_prime_numbers_asymtotic.py
@@ -4,5 +4,10 @@ from generate_prime_numbers import generate_prime_numbers_asymptotic
 class TestPrimeNumberGeneration(unittest.TestCase):
 
 	def test_one_is_not_prime_number(self):
-		
-		self.assertEqual(generate_prime_numbers_asymptotic(1), 1, "1 is not a prime number")
+
+		self.assertEqual(generate_prime_numbers_asymptotic(1),
+		 "Numbers below the number you have entered do not have prime numbers", 
+		 "0, 1 are not prime numbers")
+
+	def test_prime_number_result(self):
+		self.assertEqual(generate_prime_numbers_asymptotic(10), [2,3,5,7], "prime numbers 0 to 10: 2,3,5,7")
