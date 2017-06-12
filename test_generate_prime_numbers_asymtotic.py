fb4e80f04dabd2a8333b4d3a9c2210316039a730
HawiCaesar <brian.odhiambo932@gmail.com>
2017-06-12 20:32:00 +0300

    added a test to check if returned result is a list

--- test_generate_prime_numbers_asymtotic.py
+++ test_generate_prime_numbers_asymtotic.py
@@ -11,16 +11,21 @@ class TestPrimeNumberGeneration(unittest.TestCase):
 
 	# Check if a correct result is return of prime numbers from 0 - 10
 	def test_prime_number_result(self):
-		self.assertEqual(generate_prime_numbers_asymptotic(10), [2,3,5,7], "prime numbers 0 to 10: 2,3,5,7")
+		self.assertEqual(generate_prime_numbers_asymptotic(10), [2,3,5,7], "Prime numbers 0 - 10: [2,3,5,7]")
 
 	# Check if an error is thrown for negative values
 	def test_negative_values_in_input_returns_error(self):
 		with self.assertRaises(ValueError):
 			generate_prime_numbers_asymptotic(-20)
 
+	# Check if error is thrown for non-integer values
 	def test_input_must_be_integer(self):
 		with self.assertRaises(TypeError):
 			generate_prime_numbers_asymptotic("example")
+
+	# Check if list is returned on from prime number generation
+	def test_result_returned_is_list(self):
+		self.assertIsInstance(generate_prime_numbers_asymptotic(30), list, "Prime numbers must be in a list")
 
 
 if __name__ == '__main__':
